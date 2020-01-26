c52cf2b5af2e15afd392d1aee7f3bfb2004e93e5
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-01-26 17:36:42 +0530

    feat: add securities route with just one get endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@
 
 const portfolio = require("./portfolio.js");
 const trade = require("./trade.js");
+const security = require("./security.js");
 
 module.exports = {
 	portfolio: portfolio,
-	trade: trade
+	trade: trade,
+	security: security
 }
---
+++ src/routes/security.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const security = require('express').Router();
+const ExtendedError = require('../lib/extendedError.js');
+
+
+
+security.get('/get',async(req,res,next) => {
+	const response = {
+		code: 500,
+		payload: {
+			success: false,
+			data: null,
+			error: null
+		}
+	}
+
+	let securitiesList = null;
+
+	try{
+		let securitiesQueryFilter = {};
+		if(req.query.id){
+			//chaining a whole bunch of functions just because
+			//remove any spaces, split by comma (in case multiple), convert to objectId
+			securitiesQueryFilter._id = {
+				$in: req.query.id.replace(/\s/g,'').trim().split(',').map(_id => req.db.getObjectId(_id))
+			}
+		}
+		
+		if(req.query.ticker_symbol){
+			securitiesQueryFilter.ticker_symbol = {
+				$in: req.query.ticker_symbol.replace(/\s/g,'').trim().split(',')
+			}
+		}
+
+		if(req.query.type){
+			securitiesQueryFilter.type = {
+				$in: req.query.type.replace(/\s/g,'').trim().split(',').map(ticker => ticker.toUpperCase())
+			}	
+		}
+
+		securitiesList = await req.db.collection("securities").find(securitiesQueryFilter).toArray();
+
+		response.code=200;
+		response.payload.success = [];
+		response.payload.data = securitiesList;
+
+	}catch(e){
+		console.log(`portfolio|securities|ERROR|${e.message || "NA"}`,e);
+		if(e instanceof ExtendedError){
+			response.code = e.httpStatusCode;
+			response.payload.success=false;
+			response.payload.error = e.errorData;
+		} else {
+			response.code = 500;
+			response.payload.success=false;
+			response.payload.error = {
+				message: "INTERNAL SERVER ERROR"
+			}
+		}
+	}
+
+	res.status(response.code).send(response.payload);
+
+})
+
+module.exports = security;
